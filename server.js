5a334114e4b3751233f7edfda81f4b063b8c181e
wangyang <wyhdgx@163.com>
2022-04-23 17:21:49 +0800

    v0.1.3

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require("http");
 http.createServer(function (request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end("Hello World!\nVersion:0.1.1\nDate:2022-4-23 4:07");
+    response.writeHead(200, { 'Content-Type': 'text/HTML' });
+    response.end("<html><head></head><body><p>Hello World!</p><p>Version:0.1.3</p><p>Date:2022-4-23</p></body>");
 }).listen(8888);
 console.log("Server running at http://127.0.0.1:8888");

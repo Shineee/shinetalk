f5b6cc9903d9e39637dbddcbd23f6cc3f9d077bb
wangyang <wyhdgx@163.com>
2022-05-02 00:52:11 +0800

    v0.1.12

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,4 @@
 module.exports = {
-  apps: [{
-    name: "dev.shinetalk.wang",
-    script: 'dev-server.js',
-    watch: '.'
-  }, {
-    name: "pre.shinetalk.wang",
-    script: 'pre-server.js',
-    watch: '.'
-  }, {
-    name: "www.shinetalk.wang",
-    script: 'www-server.js',
-    watch: '.'
-  }],
-
   deploy: {
     dev: {
       user: 'shine',
@@ -21,7 +7,7 @@ module.exports = {
       repo: 'git@github.com:Shineee/shinetalk.git',
       path: '/home/shine/web/dev/shinetalk',
       'pre-deploy-local': '',
-      'post-deploy': 'pm2 startOrRestart ecosystem.config.js',
+      'post-deploy': 'pm2 startOrRestart dev-server.config.js',
       'pre-setup': 'rm -rf /home/shine/web/dev/shinetalk/source',
       'post-setup': ''
     },
@@ -32,7 +18,7 @@ module.exports = {
       repo: 'git@github.com:Shineee/shinetalk.git',
       path: '/home/shine/web/pre/shinetalk',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js',
+      'post-deploy': 'pm2 startOrRestart pre-server.config.js',
       'pre-setup': ''
     },
     www: {
@@ -42,7 +28,7 @@ module.exports = {
       repo: 'git@github.com:Shineee/shinetalk.git',
       path: '/home/shine/web/www/shinetalk',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env www',
+      'post-deploy': 'pm2 startOrRestart www-server.config.js',
       'pre-setup': ''
     }
   }
--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 module.exports={
-    version_code:"0.1.11",
-    build_time:"2022-04-30"
+    version_code:"0.1.12",
+    build_time:"2022-05-02"
 };
